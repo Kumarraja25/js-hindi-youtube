acb2dc928ac690c105875e3a8e49e741e65a4e54
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-11-14 02:46:15 +0000

    variables

---
+++ 01_Basis/01_variables.js
@@ -0,0 +1,15 @@
+const schNo = 5925
+let emailId = "rajakumar5925@gmail.com"
+var emailPass = "12345"
+homeCity = "Jalanhar"
+let homeState;
+console.log(emailPass)
+console.log(homeCity)
+
+emailPass = "526256"
+homeCity = "Sambalpur"
+
+console.table([schNo,emailId,emailPass,homeCity,homeState  ])
+/*prefer not to use var 
+because of issue in block scope and functional scope 
+*/
