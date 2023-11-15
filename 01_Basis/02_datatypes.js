eaf6fddd1af9e3e16f870d00307034bda1b3208f
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-11-15 13:54:35 +0000

    objects in datatype

--- 01_Basis/02_datatypes.js
+++ 01_Basis/02_datatypes.js
@@ -2,3 +2,20 @@
 
 //alert(3+3) //this is nodejs , not browser so no popup
 
+let n = "Raja Kumar Bisi";
+const roll = 2201109070;
+let opinion = true;
+let cr_sh;
+let l_ve = null;
+/*
+string
+number - 2 to the power 53
+boolean
+null(object)
+undefined
+symbols
+*/
+console.log(typeof cr_sh);
+console.table([n,roll,opinion,cr_sh,l_ve]);
+console.log(typeof l_ve);
+console.log(typeof opinion);
