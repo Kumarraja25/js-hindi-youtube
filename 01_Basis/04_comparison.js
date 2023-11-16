78d3d44b95f151c8e6c6fb21e03657c81a9beddf
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-11-16 01:52:56 +0000

    comparison

---
+++ 01_Basis/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null>=0)
+// true because of conversion  
+console.log(null==0)
+//false because equality operators make no conversion
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined>=0)
+
+// === strict check (datatype is also compared)
+
+console.log("2"===2)
