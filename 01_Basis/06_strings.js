859b99a2613c0c40dddbd528b5b06bac2cba5e42
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-11-25 14:22:03 +0000

    methods updated

--- 01_Basis/06_strings.js
+++ 01_Basis/06_strings.js
@@ -16,8 +16,26 @@ console.log(name.charAt(2)) ;
 console.log(name.indexOf("a"));
 
 //substrings // don't supports negative arguments
-let nName = name.substrings(0,4)
+let nName = name.substring(0,4)
 console.log(nName) ;
 //slice // supports negative arguments
-let sName = name.slices(-8,5)
+let sName = name.slice(-8,5)
 console.log(sName) ;
+
+//replace and includes 
+const url="https://Raja.com/raja%20html"
+console.log(url.replace("%20","-"));
+console.log(url.includes("Raja"));
+
+//trim, trimStart, trimEnd
+let nickName="   Nabin   ";
+console.log(nickName.trim());
+console.log(nickName.trimStart());
+console.log(nickName.trimEnd());
+
+//split
+
+let sDetails="Raja-CSE-2201109070-G:2";
+console.log(sDetails.split("-"))
+
+
