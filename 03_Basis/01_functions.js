a79c91dd094896d53988fa2b32339654f5ca6390
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-12-14 14:13:39 +0000

    rest operator

--- 03_Basis/01_functions.js
+++ 03_Basis/01_functions.js
@@ -35,4 +35,33 @@ function loginMessage2(username="sam"){
     return `${username} just logged in`
 }
 result=loginMessage2()
-console.log(result)
+// console.log(result)
+
+//Rest operator for mutiple argument passing
+function calculateCartPrice(val1,val2,...num){
+    return num;
+}
+console.log(calculateCartPrice(100,200,300,500,600));//returns a array [ 300, 500, 600 ]
+  
+const user={
+    userName:"Raja",
+    id:70
+}
+
+function handleObject(anyObject){
+    console.log(`Username id is ${anyObject.userName} and id is ${anyObject.id}`);
+}
+// handleObject(user)
+
+handleObject({
+    userName:"Raja Kumar Bisi",
+    id:2201109070
+})
+
+const myNewArray=[2,445,89]
+function returnSecondNumber(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondNumber(myNewArray));
+console.log(returnSecondNumber([200,578,789]));
