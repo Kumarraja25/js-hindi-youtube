50531149a958798aae4018bd3d4ae57aabd8501b
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-12-16 14:19:40 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while and do while ...
+
+let index =0;
+while(index<10){
+    //console.log(`Value of index is ${index}`)
+    index+=2
+}
+
+const myArray=['Batman','flash','superman']
+let arr=0;
+while(arr<myArray.length){
+    //console.log(myArray[arr])
+    arr++
+}
+
+let score=89
+do{
+   console.log(`score is ${score}`)
+   score+=2
+}while(score<=10)
