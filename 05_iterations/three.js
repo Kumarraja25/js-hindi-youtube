d568a9e7afba5c1e7f8c9923bc7064e13bca978f
Kumarraja25 <143925079+Kumarraja25@users.noreply.github.com>
2023-12-16 14:56:09 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,38 @@
+// for of
+
+let numbers=[1,2,3,4,5,6]
+for(const num of numbers){
+    //console.log(`Value is ${num}`)
+}
+
+const greetings='Hello World!'
+for(const greet of greetings){
+    if(greet==' ')
+       continue;
+    //console.log(`Character is ${greet}`)
+}
+
+// maps...
+
+const state=new Map()
+state.set('OD','Odisha')
+state.set('GJ','Gujurat')
+state.set('PJ','Punjab')
+state.set('KL','Kerela')
+
+//console.log(state)
+
+for(const s of state){
+    //console.log(s)
+}
+for(const [s,v] of state){
+    //console.log(s,':',v)
+}
+
+const myObject={
+    game1:"city racing",
+    game2:"subway suffer"
+}
+for(const [key,value] of myObject){
+   //console.log(key,':',value) //not iterable
+}
